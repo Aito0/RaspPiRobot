72bf069d56804b18e13dab88dedd7cf01a7aa9b8
Priyansh <priyanshchugh@outlook.com>
2025-01-21 18:14:09 +0000

    straight-line drive for a fixed distance

--- week-1.py
+++ week-1.py
@@ -1,24 +1,59 @@
+import math
 import brickpi3
+
 BP = brickpi3.BrickPi3()
 
 print("BrickPi3 loaded")
 
 BP.reset_all()
 
+"""
+Variables and constants
+"""
 LEFT_WHEEL = BP.PORT_D
 RIGHT_WHEEL = BP.PORT_A
 
+WHEEL_WIDTH = 999  # width between wheels TODO
+WHEEL_RADIUS = 35  # mm
+
+DPS = 45  # degrees per second
+
+DISTANCE_THRESHOLD = 0  # mm
 
 BP.set_motor_limits(LEFT_WHEEL, 70, 1000)
 BP.set_motor_limits(RIGHT_WHEEL, 70, 1000)
 
-        
-while True:
-    try:
-        BP.set_motor_dps(LEFT_WHEEL, 360)
-        BP.set_motor_dps(RIGHT_WHEEL, 360)
-    except KeyboardInterrupt:
-        BP.reset_all()
-        print("Terminated: Ctrl+C pressed")
-        exit()
+
+def turnLeft90():
+    pass
+
+
+def moveForward(dist):
+    BP.offset_motor_encoder(LEFT_WHEEL, BP.get_motor_encoder(LEFT_WHEEL))
+    BP.offset_motor_encoder(RIGHT_WHEEL, BP.get_motor_encoder(RIGHT_WHEEL))
+
+    # target = 619.4
+    target = 180 * dist / (math.pi * WHEEL_RADIUS)
+
+    left_status = BP.get_motor_status(LEFT_WHEEL)
+    right_status = BP.get_motor_status(RIGHT_WHEEL)
+
+    BP.set_motor_limits(LEFT_WHEEL, 60, 90)
+    BP.set_motor_limits(RIGHT_WHEEL, 60, 90)
+
+    while abs(left_status[2] - target) >= DISTANCE_THRESHOLD and (abs(right_status[2] - target) >= DISTANCE_THRESHOLD):
+        BP.set_motor_position(LEFT_WHEEL, target)
+        BP.set_motor_position(RIGHT_WHEEL, target)
+
+        left_status = BP.get_motor_status(LEFT_WHEEL)
+        right_status = BP.get_motor_status(RIGHT_WHEEL)
+
+        # TODO: Robot is stuck in here
+
+
+try:
+    moveForward(400)
+except KeyboardInterrupt:
+    print("Terminated: Ctrl+C pressed")
+    BP.reset_all()
 
