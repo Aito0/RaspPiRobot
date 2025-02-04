7e2cb922af840c2baad60cdb908b9d3f40168f9f
Priyansh <priyanshchugh@outlook.com>
2025-02-04 15:44:24 +0000

    data

--- covMatrix.py
+++ covMatrix.py
@@ -2,10 +2,9 @@ import numpy as np
 from typing import List
 
 def covar(xs: List[float], ys: List[float]) -> np.ndarray:
-
     return np.cov(np.array([xs, ys])) # covariance matrix
 
 # Test
-xs = [1, 2, 3, 4, 20]
-ys = [2, 3, 15, 5, 6]
-print(covar(xs, ys)) # [[62.5, 1.75], [1.75, 26.7]]
+xs = [0.4, 0.1, 0.2, 0.05, 0.6, 0.3, 0.05, 0.3, 0.5, 0.05]
+ys = [0.2, 0.1, 0.1, 0.8, 0.2, 0.4, 0.05, 0.2, 0.3, 0]
+print(covar(xs, ys))
